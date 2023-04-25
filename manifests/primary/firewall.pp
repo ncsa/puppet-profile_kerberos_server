b794582a65ec1635881e6b4e43ff8a958637fba6
Christopher Lindsey <lindsey@hort.net>
2023-04-25 15:31:44 -0500

    SVCPLAN-3447 Restrict kerberos-adm (port 749) access to certain subnets

--- manifests/primary/firewall.pp
+++ manifests/primary/firewall.pp
@@ -5,15 +5,26 @@
 #
 class profile_kerberos_server::primary::firewall {
 
-  # This should be kadmin and kpasswd
+  # kpasswd
   ['tcp','udp'].each |$protocol| {
-
-    firewall { "212 Kerberos via ${protocol}":
+    firewall { "212 kpasswd ${protocol}":
       proto  => $protocol,
-      dport  => [
-        '464',
-        '749',
-      ],
+      dport  => '464',
+      action => 'accept',
+    }
+  }
+
+  # kadmin
+  [
+    '141.142.0.0/16',
+    '10.142.0.0/16',
+    '172.24.0.0/13',
+    '172.16.0.0/13'
+  ].each |$range| {
+    firewall { "213 kerberos-adm tcp ${range}":
+      source => $range,
+      proto  => 'tcp',
+      dport  => '749',
       action => 'accept',
     }
   }
